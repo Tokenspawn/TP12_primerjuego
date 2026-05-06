fcf96ff02c90bf330bb81aa2b0f42c3f5cfb71b7
Tokenspawn <pvz2joaco@gmail.com>
2026-05-06 14:34:20 -0300

    Initial commit

---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float speed;
+    public float floorYValue;
+
+    CajaSpawner cajaSpawner;
+
+    void Start()
+    {
+        cajaSpawner = GetComponent<CajaSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -speed, 0);
+
+        if(transform.position.y <= floorYValue)
+        {
+            cajaSpawner.LocateBoxAtRandomPosition();
+        }
+    }
+}

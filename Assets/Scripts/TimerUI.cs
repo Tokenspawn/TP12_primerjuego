935867d0d4b7567c7c6414673dd1624518b3bf59
Tokenspawn <pvz2joaco@gmail.com>
2026-05-13 09:00:16 -0300

    listo punto 2

---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerUI : MonoBehaviour
+{
+    public TextMeshProUGUI txtTiempo;
+    float tiempoTranscurrido = 0f;
+
+    void Update()
+    {
+        tiempoTranscurrido += Time.deltaTime;
+        txtTiempo.text = tiempoTranscurrido.ToString("F2");
+    }
+}

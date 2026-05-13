725998ebc3fc41e36583322f0a85c59020eb9356
Tokenspawn <pvz2joaco@gmail.com>
2026-05-13 08:53:50 -0300

    aumento de veolicdad terminado

--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -5,23 +5,9 @@ using UnityEngine;
 public class CajaMovement : MonoBehaviour
 {
     public float speed;
-    public float floorYValue;
 
-    CajaSpawner cajaSpawner;
-
-    void Start()
-    {
-        cajaSpawner = GetComponent<CajaSpawner>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, -speed, 0);
-
-        if(transform.position.y <= floorYValue)
-        {
-            cajaSpawner.LocateBoxAtRandomPosition();
-        }
+        transform.Translate(0, -speed * Time.deltaTime, 0);
     }
 }
